600ab5a59da9e849c4f6488b4e3ee8f78223e6a3
JENNY PILAY <nashlyascepilay@hotmail.com>
2016-03-20 16:25:41 -0500

    correccion en el script

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 console.log("inicio del programa");
 alert ("hola javascript");
-document.write("<h1> Hola javascript! ");
+document.write("<h1> Hola javascript! </h1> ");
 alert("gracias por visitar");
-console.log("fin del programa")
+console.log("fin del programa");
