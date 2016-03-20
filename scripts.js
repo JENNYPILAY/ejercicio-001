102abb3eb5eb13a0926f81d6c4c35c247754e5a5
JENNY PILAY <nashlyascepilay@hotmail.com>
2016-03-20 16:43:28 -0500

    agregamos consolas

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 
 console.log("inicio del programa");
+var equipo= prompt ("cual es tu equipo de futbol favorit0?");
+document.write("</h1> el quipo favorito "+equipo+"</h1>");
+
 alert ("hola javascript");
 document.write("<h1> Hola javascript! </h1> ");
 alert("gracias por visitar");
 console.log("fin del programa");
+
