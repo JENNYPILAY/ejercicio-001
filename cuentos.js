22bfb133e00a3f9b6501f35f504fca83bcf9ad57
jennypilay <nashlyascepilay@hotmail.com>
2016-03-22 17:04:17 -0500

    se realizo cambios en el document write

--- cuentos.js
+++ cuentos.js
@@ -3,6 +3,6 @@ var adjetivo=prompt ("Ingreso un adjetivo");
 var sustantivo=prompt ("Ingreso un sustantivo");
 document.write("</h1> les contamos un cuento </h1>");
 document.write("<p>E1 " + sustantivo + " " + adjetivo + " " + verbo + " desesperadamente</p> ");
-document.write("<p>E1 " + verbo + "el " + sustantivo + " " + adjetivo + " desesperadamente</p> ");
+document.write("<p> " + verbo + " el " + sustantivo + " " + adjetivo + " desesperadamente</p> ");
 document.write("<p> desesperadamente el "+ adjetivo + " " + sustantivo + " " + verbo +"</p>");
 
