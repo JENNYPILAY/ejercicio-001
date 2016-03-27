e34fb7be4eb90d2f2dd1690a353d4627ea6e2a57
jennypilay <nashlyascepilay@hotmail.com>
2016-03-27 15:42:16 -0500

    numero al azar entre dos limites

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
 
-function saludo()
+
+function numALAzar(lim1,lim2)
 {
-	
-  mensaje= "hola a todos"
-	alert (mensaje);
+	var num = Math.floor(Math.random()*(lim1-lim2)) + lim2;
+    return num;
 }
-var mensaje= "chao"
-saludo();
-alert (mensaje);
-saludo();
+
+var limit1 = parseInt(prompt("ingrese primer limite mayor:"));
+if (isNaN(limit1))
+{
+	throw Error("NO ES NUMERO VALIDO");
+} 
+var limit2  = parseInt(prompt("ingrese segundo limite menor:"));
+if (isNaN(limit2))
+{
+	throw Error("NO ES UN NUMERO VALIDO");
+}
+var numero = numALAzar(limit1,limit2);
+alert("los numeros al azar son: " +numero);
+
+
+
+
+
