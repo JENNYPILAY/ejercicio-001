e0071b8864eaab11746a1077455d1181fa3bd9a4
jennypilay <nashlyascepilay@hotmail.com>
2016-03-27 12:10:10 -0500

    se agrego adivina numero

--- scripts.js
+++ scripts.js
@@ -1,6 +1,45 @@
 
 
-var dado = Math.floor(Math.random() * 6) + 1; 
- alert("Dado dice:" + dado);
-  console.log("programa completo");
-  
+console.log("inicio del programa");
+var numerousuario=prompt("ingrese numero del 1 al 5:");
+var numerodeadivinar= 3;
+if (parseInt(numerousuario)===numerodeadivinar)
+{
+	adivinacorrectamente = true;
+}
+else if  (parseInt(numerousuario) > numerodeadivinar)
+{
+	alert("el numero que inghresaste es mayor");
+	var nuevaoportunidad = prompt("intento de nuevo,ingrese un numero del 1 al 5");
+	if ((parseInt(nuevaoportunidad)===numerodeadivinar))
+	{
+		adivinacorrectamente = true;
+	}
+	else
+	{
+		adivinacorrectamente = false;
+
+	}
+}
+else if ((parseInt(numerousuario)<numerodeadivinar))
+{
+	alert("el numero que inghresaste es menor");
+	var nuevaoportunidad = prompt("intento de nuevo,ingrese un numero del 1 al 5");
+	if (parseInt(nuevaoportunidad)===numerodeadivinar)
+	{
+		adivinacorrectamente = true;
+	}
+	else
+	{
+		adivinacorrectamente = false;
+}
+}
+if (adivinacorrectamente)
+{
+document.write("<p>ADIVINASTE</p>");
+}
+else
+{
+document.write("<p>NO ADIVINASTE .intentalo denuevo</p>");
+}
+console.log ("fin del programa");
