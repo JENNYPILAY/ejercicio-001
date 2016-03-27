bf3f8f68b3edb00476d6c4f08d051c49cf137493
jennypilay <nashlyascepilay@hotmail.com>
2016-03-27 11:14:07 -0500

    se agregos contabilidad de peliculas

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 
 
 console.log ("inicio del programa");
-var usuarionumero =prompt ("INGRESE UN NUMERO:"); 
-var aleatorio = Math.floor(Math.random() *usuarionumero) + 1; 
-alert("El número aleatorio:" +aleatorio);
-console.log("programa completo");
-
+ var peliculasvistas= prompt("cuantas peliculas has visto en el mes");
+ var seriesvistas=prompt ("cuantas series has visto en el mes");
+ var tiempopantalla= parseInt(peliculasvistas)+ parseInt (seriesvistas);
+ alert ("wow! Has visto" + tiempopantalla + "veces minimo la pantalla");
+ console.log ("inicio del programa");
