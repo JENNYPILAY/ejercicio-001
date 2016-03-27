e483aa3d99efc35f5da76968e03bedc142b2872c
jennypilay <nashlyascepilay@hotmail.com>
2016-03-27 11:16:34 -0500

    se creo dado jp2

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 
 
-console.log ("inicio del programa");
- var peliculasvistas= prompt("cuantas peliculas has visto en el mes");
- var seriesvistas=prompt ("cuantas series has visto en el mes");
- var tiempopantalla= parseInt(peliculasvistas)+ parseInt (seriesvistas);
- alert ("wow! Has visto" + tiempopantalla + "veces minimo la pantalla");
- console.log ("inicio del programa");
+var dado = Math.floor(Math.random() * 6) + 1; 
+ alert("Dado dice:" + dado);
+  console.log("programa completo");
+  
