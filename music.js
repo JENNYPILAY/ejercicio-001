32218eedfc7e92722eba1a8b0f11a45e68ff0694
JENNYPILAY <nashlyascepilay@hotmail.com>
2016-04-03 12:51:34 -0500

    se agrego lista stock de productos

--- music.js
+++ music.js
@@ -2,10 +2,12 @@
 console.log("inicio del program"); 
 function printList (list) 
 {
-	var listHTML = '<ol>';
+	//var productostock=["arroz" , "aceite" ,"shampo" , "frejoles" , "acondicionador"];
+	var listHTML= "<ol>"
 	for (var i = 0; i< list.length; i ++)
 	 {
-		listHTML += '<li>' + list[i]+ '</li>';
+		listHTML += '<li>' + list[i]+ '</li>'
+
 	}
     listHTML += '</ol>';
     print(listHTML);
@@ -14,12 +16,39 @@ function print(html)
 {
 	document.write(html);
 }
-var playlist = [];
-playlist.push("violeta");
-playlist.push("tu eres bandido");
-playlist.push("el problema");
-playlist.unshift("romeo santos");
-playlist.unshift("soneros de bailadores");
+var producto = [];
+producto.push("arroz");
+producto.push("aceite");
+producto.push("shampoo");
+producto.push("frejoles");
+producto.push("acondicionador");
+
+while (true)
+{
+	var productostock=prompt ("ingrese el producto a buscar ,o s para salir:");
+	if(productostock == "s")
+	{
+		break;
+	}
+
+else if (productostock == "l")
+{
+	printList(producto);
+}
+else
+{
+var tenemos = producto.indexOf(productostock);
+if (tenemos>=0)
+{
+	alert("SI TENEMOS EL PRODUCTO EN STOCK");
+}
+else if (tenemos<0)
+{
+	alert("ESTE PRODUCTO NO LO TENEMOS");
+}
+}
+}
+
 
 printList( playlist );
 console.log("program completando")
